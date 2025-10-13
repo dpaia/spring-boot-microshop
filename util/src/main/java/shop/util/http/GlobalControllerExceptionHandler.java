961ad3aa42288ce3b7b282e82cab86fa3d867ea9
Dmitriy Chernyak <dchernyak@setronica.com>
2025-10-13 13:49:34 +0700

    Review Service: validate Review entities before persisting #7 - Add `spring-boot-starter-validation` dependency to enable bean validation - Annotate fields in `ReviewEntity` with validation constraints - Introduce `ReviewValidationTests` to validate `ReviewEntity` constraints
    We need to ensure that:
    
        productId / reviewId >= 0
        author / subject / content are not blank
        content is more than 50  and  no more than 200 characters
    
    Additionally: fixed the alignment issue and removed unwanted file change
    
    FAIL_TO_PASS: shop.microservices.core.review.PersistenceTests, shop.microservices.core.review.ReviewServiceApiTests, shop.microservices.core.review.ReviewValidationTests, shop.microservices.core.review.ReviewServiceApplicationTests
    PASS_TO_PASS: []

--- util/src/main/java/shop/util/http/GlobalControllerExceptionHandler.java
+++ util/src/main/java/shop/util/http/GlobalControllerExceptionHandler.java
@@ -3,15 +3,20 @@ package shop.util.http;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 import shop.api.exceptions.InvalidInputException;
 import shop.api.exceptions.NotFoundException;
 
+import java.util.stream.Collectors;
+
 import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 @RestControllerAdvice
@@ -34,6 +39,21 @@ class GlobalControllerExceptionHandler {
             InvalidInputException ex
     ) {
         return createHttpErrorInfo(UNPROCESSABLE_ENTITY, request, ex);
+    }
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public @ResponseBody HttpErrorInfo handleValidationExceptions(
+            ServerHttpRequest request,
+            MethodArgumentNotValidException ex
+    ) {
+        String message = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        
+        final String path = request.getPath().pathWithinApplication().value();
+        LOG.debug("Returning HTTP status: {} for path: {}, message: {}", BAD_REQUEST, path, message);
+        return new HttpErrorInfo(BAD_REQUEST, path, message);
     }
 
     private HttpErrorInfo createHttpErrorInfo(
