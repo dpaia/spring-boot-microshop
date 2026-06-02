6f6aaca4d701f4ce7c7a44816e596bf401640121
EE Bench Migration <ee-bench-migration@example.invalid>
2026-06-02 18:35:52 +0200

    Add codegen datapoint dpaia__spring__boot__microshop-20

--- microservices/review-service/src/test/java/shop/microservices/core/review/ReviewServiceApplicationTests.java
+++ microservices/review-service/src/test/java/shop/microservices/core/review/ReviewServiceApplicationTests.java
@@ -7,8 +7,11 @@ import org.springframework.core.env.Environment;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(properties = {"spring.flyway.enabled: false"})
-public class ReviewServiceApplicationTests {
+@SpringBootTest(properties = {
+        "spring.flyway.enabled: false",
+        "eureka.client.enabled=false"
+})
+public class ReviewServiceApplicationTests extends MySqlTestBase {
 
     @Autowired
     private Environment environment;
